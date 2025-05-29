eb48c5eafdde51b97b2cb4e80eff01d65122464d
TanishaKothari <kotharitanisha50@gmail.comgit config --global user.email kotharitanisha50@gmail.com>
2025-05-29 12:01:52 +1000

    Updated skills

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ document.addEventListener("DOMContentLoaded", () => {
             ]
           },
           {
-            title: "Web Development Spells",
+            title: "Full-Stack Spellbook",
             skills: [
               { name: "Python & Flask", level: 80, icon: '<i class="fab fa-python"></i>' },
               { name: "JavaScript", level: 70, icon: '<i class="fab fa-js"></i>' },
@@ -158,6 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
               { name: "C++", level: 75, icon: '<i class="fas fa-code"></i>' },
               { name: "MySQL & SQLite", level: 72, icon: '<i class="fas fa-database"></i>' },
               { name: "HTML & CSS", level: 75, icon: '<i class="fab fa-html5"></i>' },
+              { name: "Tailwind CSS", level: 60, icon: '<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.036c-2.667 0-4.333 1.325-5 3.976 1-1.325 2.167-1.822 3.5-1.491.761.189 1.305.738 1.906 1.345C13.387 10.855 14.522 12 17 12c2.667 0 4.333-1.325 5-3.976-1 1.325-2.166 1.822-3.5 1.491-.761-.189-1.305-.738-1.907-1.345-.98-.99-2.114-2.134-4.593-2.134zM7 12c-2.667 0-4.333 1.325-5 3.976 1-1.326 2.167-1.822 3.5-1.491.761.189 1.305.738 1.907 1.345.98.989 2.115 2.134 4.594 2.134 2.667 0 4.333-1.325 5-3.976-1 1.325-2.167 1.822-3.5 1.491-.761-.189-1.305-.738-1.906-1.345C10.613 13.145 9.478 12 7 12z"/></svg>' },
               { name: "Bootstrap", level: 70, icon: '<i class="fab fa-bootstrap"></i>' }
             ]
           },
